fffc2f97ed1fe6f69448ff994db3cfa1fe467caa
Hekmon <edouardhur@gmail.com>
2020-05-24 16:15:14 +0200

    add the base controller

--- controller.go
+++ controller.go
@@ -1,1 +1,46 @@
 package qbtapi
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/hashicorp/go-cleanhttp"
+)
+
+// New return a initialized and ready to use Controller.
+// customHTTPClient can be nil
+func New(apiEndpoint *url.URL, user, password string, customHTTPClient *http.Client) (c *Controller, err error) {
+	// handle url
+	if apiEndpoint == nil {
+		err = errors.New("apiEndpoint can't be nil")
+		return
+	}
+	copiedURL, err := url.Parse(apiEndpoint.String())
+	if err != nil {
+		err = fmt.Errorf("apiEndpoint can't be (re)parsed as URL: %v", err) // weird
+		return
+	}
+	// handle http client
+	if customHTTPClient == nil {
+		customHTTPClient = cleanhttp.DefaultPooledClient()
+	}
+	// spawn the controller
+	c = &Controller{
+		user:     user,
+		password: password,
+		url:      copiedURL,
+		client:   customHTTPClient,
+	}
+	return
+}
+
+// Controller is a statefull object allowing to interface the qBittorrent Web API on a particular endpoint.
+// Must be instanciated with New().
+type Controller struct {
+	user     string
+	password string
+	url      *url.URL
+	client   *http.Client
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hekmon/go-qbittorrent-webapi
 
 go 1.14
+
+require github.com/hashicorp/go-cleanhttp v0.5.1
